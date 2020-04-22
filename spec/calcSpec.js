db496292d2d2968eb608004bdb675cb861d93791
S10N <sion.jeremiah@gmail.com>
2020-04-22 14:23:32 +0000

    220420 final push today no updates

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,16 +1,18 @@
 
 describe("Calculator", function() {
-    describe("Addition tests", function() {
+    describe("Addition function", function() {
         it("should return 42", function() {
             expect(addition(20, 22)).toBe(42);
         });
+
         it("should return 26", function() {
             expect(addition(7, 19)).toBe(26);
-            });
-            it("Should return an error if we don't supply two numbers", function() {
-            SpyOn(window, "alert");
-            addition("Hitchhikers", "Guide");
-            expect(window.alert).toHaveBeenCalledWith ("Error!");
-            });
         });
+
+        it("should return an error if we don't supply two numbers", function() {
+            spyOn(window, "alert");
+            addition("Hitchhikers", "Guide")
+            expect(window.alert).toHaveBeenCalledWith("Error!");
+        });
+    });
 });
